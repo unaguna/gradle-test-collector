62292a6bbe725848c5e09a4936f5c7d0670d09ab
unaguna <k.izumi.ysk@gmail.com>
2022-03-13 14:19:50 +0900

    📝Write `--help` document

--- run-each-test.sh
+++ run-each-test.sh
@@ -42,8 +42,16 @@ function usage_exit () {
 
 function echo_help () {
     echo "Usage:" "$(basename "$0") -d <output_directory> [--rerun-tests] <main_project_directory>" 1>&2
-    echo ""
-    # TODO: output more help
+    echo "" 1>&2
+    echo "Options" 1>&2
+    echo "    -d <output_directory> :" 1>&2
+    echo "         (Required) Path of the directory where the results will be output." 1>&2
+    echo "    --rerun-tests :" 1>&2
+    echo "         If it is specified, tests that have already been run are also rerun." 1>&2
+    echo "" 1>&2
+    echo "Arguments" 1>&2
+    echo "    <main_project_directory> :" 1>&2
+    echo "         (Required) Path of the root directory of the gradle project." 1>&2
 }
 
 # Output an information
