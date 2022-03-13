ac15758949bbe9b415339f3fda9f4938ee88a2fb
unaguna <k.izumi.ysk@gmail.com>
2022-03-13 23:07:13 +0900

    ♻Unify scripts

--- run-each-test.sh
+++ run-each-test.sh
@@ -63,11 +63,21 @@ function echo_info () {
     echo "$SCRIPT_NAME: " "$@" >&2
 }
 
+# Output an error
+#
+# Because stdout is used as output of gradlew in this script,
+# any messages should be output to stderr.
+function echo_err() {
+    echo "$SCRIPT_NAME: " "$@" >&2
+}
+
 ################################################################################
 # Constant values
 ################################################################################
 
-readonly GET_SUMMARY_SCRIPT="$SCRIPT_DIR/test-summary.sh"
+readonly PRINT_LINE_PY="$SCRIPT_DIR/get-summary.py"
+
+readonly CREATE_REPORT_INDEX="$SCRIPT_DIR/create-report-index.py"
 
 
 ################################################################################
@@ -166,6 +176,16 @@ tmp_project_list_path=$(mktemp)
 readonly tmp_project_list_path
 tmpfile_list+=( "$tmp_project_list_path" )
 
+# the output of `gradle tasks`
+tmp_tasks_path=$(mktemp)
+readonly tmp_tasks_path
+tmpfile_list+=( "$tmp_tasks_path" )
+
+# the list of HTML report
+tmp_summary_path=$(mktemp)
+readonly tmp_summary_path
+tmpfile_list+=( "$tmp_summary_path" )
+
 
 ################################################################################
 # main
@@ -174,6 +194,9 @@ tmpfile_list+=( "$tmp_project_list_path" )
 cd "$main_project_dir"
 
 readonly stdout_dir="$output_dir/stdout"
+readonly summary_path="$output_dir/summary.txt"
+readonly output_report_dir="$output_dir/test-report"
+readonly output_xml_dir="$output_dir/xml-report"
 
 # create the directory where output
 if [ -n "$output_dir" ]; then
@@ -181,6 +204,12 @@ if [ -n "$output_dir" ]; then
 fi
 if [ -n "$stdout_dir" ]; then
     mkdir "$stdout_dir"
+fi
+if [ -n "$output_report_dir" ]; then
+    mkdir "$output_report_dir"
+fi
+if [ -n "$output_xml_dir" ]; then
+    mkdir "$output_xml_dir"
 fi
 
 # Get sub-projects list
@@ -225,5 +254,57 @@ find . -type d -name node_modules -prune -o -type f -name 'build.gradle*' -print
 done
 
 
-# Aggregate the test results
-"$GET_SUMMARY_SCRIPT" -d "$output_dir" "$main_project_dir"
+# get task list
+./gradlew tasks --all < /dev/null | awk -F ' ' '{print $1}' >> "$tmp_tasks_path"
+
+
+# Read each build.gradle and copy test reports.
+find . -type f -name 'build.gradle*' -print | while read -r project_file; do
+    project_dir=$(dirname "$project_file")
+    project_name=$(sed -e "s|/|:|g" -e "s|^\.||" <<< "$project_dir")
+    project_name_esc=${project_name//:/__}
+    test_result_xml_dir="$project_dir/build/test-results/test"
+    test_result_html_dir="$project_dir/build/reports/tests/test"
+    test_result_xml_tar="$output_xml_dir/${project_name_esc:-"root"}.tgz"
+    test_result_html_dist_dir="$output_report_dir/${project_name_esc:-"root"}"
+    go_mod_path="$project_dir/go.mod"
+
+    # Even if the build.gradle file exists, 
+    # ignore it if it is not recognized as a sub project by the root project.
+    if ! is_sub_project "$project_name" "$tmp_project_list_path"; then
+        continue
+    fi
+
+    if ! task_exists "$project_name:test" "$tmp_tasks_path"
+    then
+        echo "${project_name:-"root"}" NO-TASK >> "$tmp_summary_path"
+    elif [ ! -e "$test_result_xml_dir" ]; then
+        if [ -e "$go_mod_path" ]; then
+            echo "${project_name:-"root"}" GO >> "$tmp_summary_path"
+        elif [ "$project_name" == ":testing:integration-tests" ]; then
+            echo "${project_name:-"root"}" INTEGRATION-TEST >> "$tmp_summary_path"
+        else
+            echo "${project_name:-"root"}" NO-TESTS >> "$tmp_summary_path"
+        fi
+    else
+        # Count tests and print it
+        row_data=$(find "$test_result_xml_dir" -name '*.xml' -print0 | xargs -0 "$PRINT_LINE_PY")
+        echo "${project_name:-"root"}" "$row_data" >> "$tmp_summary_path"
+
+        # Collect the XML test report
+        (
+            cd "$test_result_xml_dir" &&
+            find . -name '*.xml' -print0 | xargs -0 tar -czf "$test_result_xml_tar"
+        )
+
+        # Collect the HTML test report
+        cp -irp "$test_result_html_dir" "$test_result_html_dist_dir"
+    fi
+done
+
+# Output the summary file
+sort "$tmp_summary_path" -o "$tmp_summary_path"
+cp "$tmp_summary_path" "$summary_path"
+
+# Output index page of HTML reports
+"$CREATE_REPORT_INDEX" "$tmp_summary_path" "$output_report_dir"
