86de2e16a7a8ba12f854469fbe57880776f2517a
unaguna <k.izumi.ysk@gmail.com>
2022-04-21 22:10:20 +0900

    🩹Follow symbolic links to find the script's installation directory
    To allow scripts to work via symbolic links.

--- src/collect-tests.sh
+++ src/collect-tests.sh
@@ -14,14 +14,46 @@ set -C
 # Script information
 ################################################################################
 
+# Readlink recursively
+# 
+# This can be achieved with `readlink -f` in the GNU command environment,
+# but we implement it independently for mac support.
+#
+# Arguments
+#   $1 - target path
+#
+# Standard Output
+#   the absolute real path
+function itr_readlink() {
+    local target_path=$1
+
+    (
+        cd "$(dirname "$target_path")"
+        target_path=$(basename "$target_path")
+
+        # Iterate down a (possible) chain of symlinks
+        while [ -L "$target_path" ]
+        do
+            target_path=$(readlink "$target_path")
+            cd "$(dirname "$target_path")"
+            target_path=$(basename "$target_path")
+        done
+
+        echo "$(pwd -P)/$target_path"
+    )
+}
+
 # The current directory when this script started.
 ORIGINAL_PWD=$(pwd)
 readonly ORIGINAL_PWD
+# The path of this script file
+SCRIPT_PATH=$(itr_readlink "$0")
+readonly SCRIPT_PATH
 # The directory path of this script file
-SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+SCRIPT_DIR=$(cd "$(dirname "$SCRIPT_PATH")"; pwd)
 readonly SCRIPT_DIR
 # The path of this script file
-SCRIPT_NAME=$(basename "$0")
+SCRIPT_NAME=$(basename "$SCRIPT_PATH")
 readonly SCRIPT_NAME
 
 # The version number of this application
