21194520f3ff0e0dcc539c881f4b0fbcc93d5bd0
unaguna <k.izumi.ysk@gmail.com>
2022-04-26 20:26:35 +0900

    ✅`summary.txt` shows status of each test task

---
+++ spec/resources/prj-100-110/mod1/src/test/java/jp/unaguna/example/Mod1Test.java
@@ -0,0 +1,14 @@
+package jp.unaguna.example;
+
+import org.junit.jupiter.api.Test;
+
+public class Mod1Test {
+    @Test
+    public void test1() {
+        // pass
+    }
+    @Test
+    public void test2() {
+        assert false;
+    }
+}
--- spec/summary_txt_spec.sh
+++ spec/summary_txt_spec.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+%const FIELD_PROJECT_NAME: 1
+%const FIELD_BUILD_STATUS: 2
+%const FIELD_TASK_STATUS: 3
+%const FIELD_TEST_STATUS: 4
+
 Describe 'summary.txt, output of collect-tests.sh,'
     BeforeAll 'install_app'
     AfterAll 'uninstall_app'
@@ -30,5 +35,92 @@ Describe 'summary.txt, output of collect-tests.sh,'
         The word 7 of line 2 of contents of file summary-file should equal 2
         The word 8 of line 2 of contents of file summary-file should equal 0
         The word 9 of line 2 of contents of file summary-file should equal 4
+    End
+
+    ExampleGroup 'status'
+        Example 'when tests pass'
+            Path summary-file=result/summary.txt
+            project='prj-100-110'
+
+            deploy_prj "$project" .
+
+            When run collect-tests.sh -d result "$project"
+            The status should equal 0
+            The error should include ':mod0:test'
+
+            The file summary-file should be file
+            The word "$FIELD_PROJECT_NAME" of line 1 of contents of file summary-file should equal ":mod0"
+            The word "$FIELD_BUILD_STATUS" of line 1 of contents of file summary-file should equal "SUCCESSFUL"
+            The word "$FIELD_TASK_STATUS" of line 1 of contents of file summary-file should equal "null"
+            The word "$FIELD_TEST_STATUS" of line 1 of contents of file summary-file should equal "passed"
+        End
+
+        Example 'when tests fail'
+            Path summary-file=result/summary.txt
+            project='prj-100-110'
+
+            deploy_prj "$project" .
+
+            When run collect-tests.sh -d result "$project"
+            The status should equal 0
+            The error should include ':mod1:test'
+
+            The file summary-file should be file
+            The word "$FIELD_PROJECT_NAME" of line 2 of contents of file summary-file should equal ":mod1"
+            The word "$FIELD_BUILD_STATUS" of line 2 of contents of file summary-file should equal "FAILED"
+            The word "$FIELD_TASK_STATUS" of line 2 of contents of file summary-file should equal "FAILED"
+            The word "$FIELD_TEST_STATUS" of line 2 of contents of file summary-file should equal "failed"
+        End
+
+        Example 'when test task is skipped'
+            Path summary-file=result/summary.txt
+            project='prj-skipped'
+
+            deploy_prj "$project" .
+
+            When run collect-tests.sh -d result "$project"
+            The status should equal 0
+            The error should include ':mod0:test'
+
+            The file summary-file should be file
+            The word "$FIELD_PROJECT_NAME" of line 1 of contents of file summary-file should equal ":mod0"
+            The word "$FIELD_BUILD_STATUS" of line 1 of contents of file summary-file should equal "SUCCESSFUL"
+            The word "$FIELD_TASK_STATUS" of line 1 of contents of file summary-file should equal "SKIPPED"
+            The word "$FIELD_TEST_STATUS" of line 1 of contents of file summary-file should equal "NO-RESULT"
+        End
+
+        Example 'when test task has no source'
+            Path summary-file=result/summary.txt
+            project='prj-no-source'
+
+            deploy_prj "$project" .
+
+            When run collect-tests.sh -d result "$project"
+            The status should equal 0
+            The error should include ':mod0:test'
+
+            The file summary-file should be file
+            The word "$FIELD_PROJECT_NAME" of line 1 of contents of file summary-file should equal ":mod0"
+            The word "$FIELD_BUILD_STATUS" of line 1 of contents of file summary-file should equal "SUCCESSFUL"
+            The word "$FIELD_TASK_STATUS" of line 1 of contents of file summary-file should equal "NO-SOURCE"
+            The word "$FIELD_TEST_STATUS" of line 1 of contents of file summary-file should equal "NO-RESULT"
+        End
+
+        Example 'when test task does not exist'
+            Path summary-file=result/summary.txt
+            project='prj-no-task'
+
+            deploy_prj "$project" .
+
+            When run collect-tests.sh -d result "$project"
+            The status should equal 0
+            The error should include ':mod0:test'
+
+            The file summary-file should be file
+            The word "$FIELD_PROJECT_NAME" of line 1 of contents of file summary-file should equal ":mod0"
+            The word "$FIELD_BUILD_STATUS" of line 1 of contents of file summary-file should equal "NO-TASK"
+            The word "$FIELD_TASK_STATUS" of line 1 of contents of file summary-file should equal "null"
+            The word "$FIELD_TEST_STATUS" of line 1 of contents of file summary-file should equal "NO-TASK"
+        End
     End
 End
