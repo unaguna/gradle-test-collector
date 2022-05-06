affb9c90db2a76a84a982a9a99969f29e15adeff
unaguna <k.izumi.ysk@gmail.com>
2022-05-06 12:42:37 +0900

    ✅`collect-tests.sh` outputs gtc-version.txt

--- spec/collect-test_output_version_spec.sh
+++ spec/collect-test_output_version_spec.sh
@@ -8,6 +8,36 @@ Describe 'collect-tests.sh' output-version
     BeforeEach 'setup'
     AfterEach 'clean'
 
+    It 'shows the version of this tool'
+        Path version-file=result/gtc-version.txt
+        project='prj-1000-1100'
+
+        deploy_prj "$project" .
+
+        When run collect-tests.sh -d result "$project" 
+        The status should equal 0
+        The output should equal ''
+        The error should include 'Collecting the test results'
+        
+        The file version-file should be exist
+        The contents of file version-file should start with 'Gradle Test Collector'
+    End
+
+    It 'shows the version of this tool even if gradle task failed'
+        Path version-file=result/gtc-version.txt
+        project='prj-all-task-broken'
+
+        deploy_prj "$project" .
+
+        When run collect-tests.sh -d result "$project" 
+        The status should equal 1
+        The output should equal ''
+        The error should include 'is broken for test'
+        
+        The file version-file should be exist
+        The contents of file version-file should start with 'Gradle Test Collector'
+    End
+
     It 'shows the version of gradle'
         Path version-file=result/gradle-version.txt
         project='prj-1000-1100'
