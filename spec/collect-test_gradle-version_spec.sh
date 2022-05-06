6dcf0a49ba77564ea960cfa3c32e31951f37402f
unaguna <k.izumi.ysk@gmail.com>
2022-05-06 11:20:43 +0900

    ✅`collect-tests.sh` outputs gradle-version.txt

---
+++ spec/collect-test_gradle-version_spec.sh
@@ -0,0 +1,40 @@
+#shellcheck shell=sh
+
+set -e
+
+Describe 'collect-tests.sh' gradle-version
+    BeforeAll 'install_app'
+    AfterAll 'uninstall_app'
+    BeforeEach 'setup'
+    AfterEach 'clean'
+
+    It 'shows the version of gradle'
+        Path version-file=result/gradle-version.txt
+        project='prj-1000-1100'
+
+        deploy_prj "$project" .
+
+        When run collect-tests.sh -d result "$project" 
+        The status should equal 0
+        The output should equal ''
+        The error should include 'Collecting the test results'
+        
+        The file version-file should be exist
+        The contents of file version-file should include 'Gradle'
+    End
+
+    It 'shows the version of gradle even if gradle task failed'
+        Path version-file=result/gradle-version.txt
+        project='prj-all-task-broken'
+
+        deploy_prj "$project" .
+
+        When run collect-tests.sh -d result "$project" 
+        The status should equal 1
+        The output should equal ''
+        The error should include 'is broken for test'
+        
+        The file version-file should be exist
+        The contents of file version-file should include 'Gradle'
+    End
+End
